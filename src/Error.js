570ac3fcbb9c0cff816462855271f09182b7a62c
Rakib-cloud <ri1641715@gmail.com>
2022-06-19 14:18:08 +0600

    calander make

---
+++ src/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1>you have already an event on this date</h1>
+        </div>
+    );
+};
+
+export default Error;
